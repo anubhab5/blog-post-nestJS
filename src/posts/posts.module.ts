ff1c95f0fe440dd83d4554365d013be5525ca597
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2024-10-19 21:36:59 +0530

    update post module with metaOption module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -5,10 +5,15 @@ import { UsersModule } from "src/users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Post } from "./entity/post.entity";
 import { MetaOptions } from "src/meta-options/entity/meta-options.entity";
+import { MetaOptionsModule } from "src/meta-options/meta-options.module";
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [UsersModule, TypeOrmModule.forFeature([Post, MetaOptions])],
+  imports: [
+    UsersModule,
+    MetaOptionsModule,
+    TypeOrmModule.forFeature([Post, MetaOptions]),
+  ],
 })
 export class PostsModule {}
